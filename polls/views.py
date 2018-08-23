7bfeeb7ee419ac07e1c2dbb7bb5b65f2711f90ec
Ped <niwatdarap3@gmail.com>
2018-08-23 12:02:05 +0700

    Changed the HTML for the site.

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,4 @@ from django.http import HttpResponse
 
 #HttpResponse("Hello, world. You're at the polls index.")
 def index(request):
-    #print("----request = "+request)
     return HttpResponse("Hello, world. You're at the polls index.")

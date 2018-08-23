be2e50e04dec73ab925a98052f6ff479afde4fed
Ped <niwatdarap3@gmail.com>
2018-08-23 10:45:53 +0700

    My dj app, edit file

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 #HttpResponse("Hello, world. You're at the polls index.")
-def index():
+def index(request):
     #print("----request = "+request)
     return HttpResponse("Hello, world. You're at the polls index.")

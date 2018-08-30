08866585d5f4de223df1374e92b518e11f840a62
Ped <niwatdarap3@gmail.com>
2018-08-30 11:58:53 +0700

    some meaningful message goes here

--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,15 @@ import webbrowser as wb
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
 def index(request):
     #https://access.line.me/oauth2/v2.1/login?returnUri=%2Foauth2%2Fv2.1%2Fauthorize%2Fconsent%3Fscope%3Dprofile%26response_type%3Dcode%26state%3D12345abcde%26redirect_uri%3Dhttp%253A%252F%252F35.197.130.54%252Fcallback.html%26client_id%3D1594794852&loginChannelId=1594794852&loginState=9pSz0AXSXEaDkgfjaZqfxo
     #wb.open_new_tab('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://35.197.130.54/callback.html')
-    return render(request, 'index.html', context=None)
+    #return render(request, 'index.html', context=None)
     #HttpResponse("Hello, world. xxx")
     #return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponseRedirect('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://niwat.pythonanywhere.com/polls/callback/&state=12345abcde&scope=profile')
+    
 
 #def about(request):
 #    return render(request, 'about.html', context=None)
@@ -18,7 +21,7 @@ def callback(request):
     #return render(request, 'index.html', context=None)
     #https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://niwat.pythonanywhere.com/polls/callback/&state=12345abcde&scope=profile
     #http://niwat.pythonanywhere.com/polls/callback/?code=PzM1z4J3qpW9zWaNMx5e&state=12345abcde
-    wb.open('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://niwat.pythonanywhere.com/polls/callback/&state=12345abcde&scope=profile')
+    #wb.open('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://niwat.pythonanywhere.com/polls/callback/&state=12345abcde&scope=profile')
     return render(request, 'about.html', context=None)
         
 #class HomePageView(TemplateView):

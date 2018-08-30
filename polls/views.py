5c737e1209c86447770402dd87b7b3bfa3f171cc
Ped <niwatdarap3@gmail.com>
2018-08-30 16:09:41 +0700

    some meaningful message goes here

--- polls/views.py
+++ polls/views.py
@@ -4,15 +4,37 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
+import requests
+
 def index(request):
     #https://access.line.me/oauth2/v2.1/login?returnUri=%2Foauth2%2Fv2.1%2Fauthorize%2Fconsent%3Fscope%3Dprofile%26response_type%3Dcode%26state%3D12345abcde%26redirect_uri%3Dhttp%253A%252F%252F35.197.130.54%252Fcallback.html%26client_id%3D1594794852&loginChannelId=1594794852&loginState=9pSz0AXSXEaDkgfjaZqfxo
     #wb.open_new_tab('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://35.197.130.54/callback.html')
-    #return render(request, 'index.html', context=None)
+    render(request, 'index.html', context=None)
     #HttpResponse("Hello, world. xxx")
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    return HttpResponseRedirect('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://niwat.pythonanywhere.com/polls/callback/&state=12345abcde&scope=profile')
+    #HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponseRedirect('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1594794852&redirect_uri=http://niwat.pythonanywhere.com/polls/callback/&state=12345abcde&scope=openid')
     
+def requestCurl():
+    data = {
+            "grant_type": "authorization_code",
+            "code":"fr1FBb3equGxa8QAMVeZ",
+            "redirect_uri":"http://niwat.pythonanywhere.com/polls/callback/",
+            "client_id":"1594794852",
+            "client_secret":"31d3e4e088773ca7feaed23cd5ecc33e"
+            }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    
+    data2 = [
+      ('grant_type', 'authorization_code'),
+      ('code', 'xxx'),
+      ('redirect_uri', 'xxx'),
+      ('client_id', 'xxx'),
+      ('client_secret', 'xxx'),
+    ]
 
+    response = requests.post('https://api.line.me/oauth2/v2.1/token', headers=headers, data=data)
+    print ('response = '+response.text)
+#requestCurl()    
 #def about(request):
 #    return render(request, 'about.html', context=None)
 def callback(request):
